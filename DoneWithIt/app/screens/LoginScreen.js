bb784f648905e8e7db070c93d9157df945fbcec5
JoeySpencer26 <spenc173@purdue.edu>
2022-10-06 22:41:51 -0400

    Login button on login screen sends to google.

--- DoneWithIt/app/screens/LoginScreen.js
+++ DoneWithIt/app/screens/LoginScreen.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';  //status-bar replaced with 'react'
 import { StyleSheet, Text, View, Button } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
+
 
 function LoginScreen({navigation}) {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '680747377509-nhf0jt64eghn93bcmanicj7a2aqok75q.apps.googleusercontent.com'
+  });
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+    }
+  }, [response]);
     return (
     <View style={styles.container}>
       <Text>Login</Text>
       <Button
-        title="Login (replace)"
-        onPress={() => navigation.navigate("BottomTabs")}
-      />
+    disabled={!request}
+    title="Login"
+    onPress={() => {
+      promptAsync();
+    }}
+  />
     </View>
     );
 }
