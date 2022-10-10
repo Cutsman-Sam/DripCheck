7429fb28c0d82850498e4a41283e841044e89c88
Justin L <jdawgs009@gmail.com>
2022-10-09 20:21:18 -0400

    Added remaining mongoose schema necessary for outfits to model folder TODO:fix js bug, finish server

--- DoneWithIt/server.js
+++ DoneWithIt/server.js
@@ -1,28 +1,37 @@
-//server global variables
-const { appendFile } = require('fs');
-const http = require('http');
-const hostname = '127.0.0.1'; //default localhost
-const PORT = 3000;
+//imports
+const os = require('os');
+const path = require('path');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitted{};
+
+//initialize objects
+const myEmitter = new Emitter();
+
+const hostname = '127.0.0.1'; //default localhost
+const PORT = process.env.PORT || 3400;
+
 const connectDB = require('./config/dbConn');
 
 //connect to DB
 connectDB();
 
-
-const server = http.createServer((req, res) => {
-    console.log('request made.');
+//check connecting to proper OS
+const server = http.createServer((request, response) => {
+    console.log(req.url, req.method);
     res.statusCode = 200; //successful response code
-
+    
     //set header content type
     res.setHeader('Content-Type', 'text/plain');
     res.write('Yo');
     res.end('Hello World'); //    
 });
 
-server.listen(port,hostname,() => {
-    console.log('Server running at http://${hostname}:${PORT}/');
-});
+server.listen(PORT,() => console.log('Server running at http://${hostname}:${PORT}/'));
 
 
 
