a6c20bae40a4b4d85130e44fb5212d1c8b5e75f9
Justin L <jdawgs009@gmail.com>
2022-10-12 14:22:43 -0400

    latest push so I can work on my desktop :/

--- DoneWithIt/server.js
+++ DoneWithIt/server.js
@@ -1,42 +1,58 @@
 //imports
 const os = require('os');
 const path = require('path');
+const net = require('net');
 const mongoose = require('mongoose');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
 const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitted{};
 
-//initialize objects
-const myEmitter = new Emitter();
-
-const hostname = '127.0.0.1'; //default localhost
-const PORT = process.env.PORT || 3400;
+//global values
+const PORT = 3400;
 
 const connectDB = require('./config/dbConn');
 
 //connect to DB
 connectDB();
 
-//check connecting to proper OS
-const server = http.createServer((request, response) => {
-    console.log(req.url, req.method);
-    res.statusCode = 200; //successful response code
-    
-    //set header content type
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Yo');
-    res.end('Hello World'); //    
-});
-
-server.listen(PORT,() => console.log('Server running at http://${hostname}:${PORT}/'));
-
-
+//create server
+const server = net.createServer(onClientConnection);
 
 //check that mongo connected before listen
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoBongo Batadase');
-    app.listen(PORT, () => console.log('Server running on port ${PORT}'))
+    
+    //start listening
+    server.listen(PORT,function() {
+        console.log(`Server started on port ${PORT}`);
+    });
 });
+
+//function to handle client connection
+function onClientConnection(socket) {
+    //log connection
+    console.log(`${sock.remoteAddress}:${sock.remotePort} Connected`);
+
+    //Handle the client data.
+    sock.on('data',/*insert data handling function here*/function(data){
+        //Log data received from the client
+        console.log(`>> data received : ${data} `);
+		
+		//prepare and send a response to the client 
+		let serverResp = "Hello from the server"
+        //setup response
+		sock.write(serverResp);
+		
+		//close the connection 
+		sock.end()    
+    });    
+
+    //Handle when client connection is closed
+    sock.on('close',function(){
+        console.log(`${sock.remoteAddress}:${sock.remotePort} Connection closed`);
+    });
+    
+	//Handle Client connection error.
+    sock.on('error',function(error){
+        console.error(`${sock.remoteAddress}:${sock.remotePort} Connection Error ${error}`);
+    });
+	
+};
