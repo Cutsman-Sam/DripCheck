f6a1dc43450b60ef3e84105419f390643d1df1f7
JustinQxc <62610088+JustinQxc@users.noreply.github.com>
2022-10-13 17:45:45 -0400

    added more comments to request handler

--- DoneWithIt/Server/requestHandler.java
+++ DoneWithIt/Server/requestHandler.java
@@ -16,54 +16,71 @@ public class requestHandler {
         switch (requestNum) {
             /* 
              * Check User Exists:
-             * Format: {"success" || "fail"}~|`1
+             * Format: 1~|`email
             */
             case 1: 
 
                 break;
             /*
              * Add new User
-             * Format: {"success" || "fail"}~|`userID~|`2
+             * Format: 2~|`email~|`displayName~|`
             */
             case 2: 
 
                 break;
             /*
-             * 
+             * Get existing user data
+             * Format: 3~|`email
              */
             case 3: 
 
                 break;
             /*
-             * 
+             * Update display name
+             * Format: 4~|`userID~|`displayName
              */
             case 4: 
 
                 break;
             /*
-             * 
+             * Update profile pic
+             * Format: 5~|`userID~|`{need to figure out what to do with pics for the moment}
              */
             case 5: 
 
                 break;
             /*
-             * 
+             * Remove all user data 
+             * Format: 6~|`userID
              */
             case 6: 
 
                 break;
             /*
-             * 
+             * Add new outfit to database under user
+             * Format: 7~|`userID~|`{outfit image idk what to do so far}~|`outfitName~|`outfitDescription
              */
             case 7: 
 
                 break;
+            /*
+             * Delete outfit from database
+             * Format: 8~|`userID~|`outfitID
+             */
             case 8: 
 
                 break;
+            /*
+             * Edit outfit information in database
+             * Format: 9~|`userID~|`outfitID~|`{outfit Image}~|`outfitName~|`outfitDescription
+            */
             case 9: 
 
                 break;
+            /*
+             * Give/receive notification token
+             * Format: 10~|`userID~|`token/NULL
+             */
             case 10: 
 
                 break;
