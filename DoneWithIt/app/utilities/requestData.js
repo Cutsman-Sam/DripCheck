a94bc4218cd1ef4cbba80fd527b03a112f22fada
JoeySpencer26 <spenc173@purdue.edu>
2022-10-30 21:59:43 -0400

    RequestData gets sample user data from mongo

--- DoneWithIt/app/utilities/requestData.js
+++ DoneWithIt/app/utilities/requestData.js
@@ -1,19 +1,19 @@
 import { sendEmail } from "./sendEmail"
-
+const KEY = 'nsGQLXniFr1RwE6idSX7fNOWIw5dZOWm3xV0TyyGTfbx5FOtQTbcyV8VDKyfYXsA';
 //
 export async function requestData(emailAddress) {
     //TODO: Gather all of user's data from database
-    const url = 'https://data.mongodb-api.com/app/<Data API App ID>/endpoint/data/v1/action/findOne';
+    const url = 'https://data.mongodb-api.com/app/data-ndazo/endpoint/data/v1/action/findOne';
 
     const options = {
         method: 'POST',
         headers: {
             'content-type': 'application/json',
-            'api-key': 'API-KEY-HERE',
+            'api-key': KEY,
         },
-        body: '{"dataSource": "<cluster name>", "database": "learn-data-api", "collection": "people", "filter": { "name": "John Sample" }}'
+        body: '{"dataSource": "DripCheckApp", "database": "test", "collection": "users", "filter": { "email": "jdawgs009@gmail.com" }}'
     };
-    const stat = (await fetch(url, options)).status
-    formattedData = "Sample User Data: qwoijciwqojrowiqejiowqjfoiqwejfiojwqeiofjwqfiowjfoiwjfewqofvoiqnifnqwifojfqweiotjwqiyeqrjiofksdamcioqwemoiw"
-    sendEmail(emailAddress, "Dripcheck: User Data Requested", formattedData)
+    let response = await fetch(url, options)
+    let data = await response.json();
+    console.log(data);
 }
