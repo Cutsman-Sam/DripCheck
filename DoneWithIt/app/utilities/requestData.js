b3227267ccbe0ea08b14728830dde1fbd2e21e48
JoeySpencer26 <spenc173@purdue.edu>
2022-11-01 22:14:15 -0400

    Skeleton of getOutfits function

--- DoneWithIt/app/utilities/requestData.js
+++ DoneWithIt/app/utilities/requestData.js
@@ -2,7 +2,6 @@ import { sendEmail } from "./sendEmail"
 const KEY = 'nsGQLXniFr1RwE6idSX7fNOWIw5dZOWm3xV0TyyGTfbx5FOtQTbcyV8VDKyfYXsA';
 //
 export async function requestData(emailAddress) {
-    //TODO: Gather all of user's data from database
     const url = 'https://data.mongodb-api.com/app/data-ndazo/endpoint/data/v1/action/findOne';
 
     const options = {
@@ -19,7 +18,6 @@ export async function requestData(emailAddress) {
 }
 
 export async function insertNewUser(email, displayName) {
-     //TODO: Gather all of user's data from database
     var date = new Date();
     var dd = String(date.getDate()).padStart(2, '0');
     var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -39,4 +37,21 @@ export async function insertNewUser(email, displayName) {
      let response = await fetch(url, options)
      let data = await response.json();
      return data;
+}
+
+export async function getOutfits(emailAddress) {
+    //TODO: Make db function to gather all outfits belonging to a specific user
+    const url = 'https://data.mongodb-api.com/app/data-ndazo/endpoint/data/v1/action/getOutfits';
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'api-key': KEY,
+        },
+        body: '{"dataSource": "DripCheckApp", "database": "test", "collection": "users", "filter": { "email": "' + emailAddress + '" }}'
+    };
+    let response = await fetch(url, options)
+    let data = await response.json();
+    return data;
 }
