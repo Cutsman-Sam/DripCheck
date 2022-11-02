1b2f9d0777745bfaba2f89c132e6e41c937e4587
JustinQxc <62610088+JustinQxc@users.noreply.github.com>
2022-11-02 00:03:44 -0400

    userExists function fully implemented, insert new outfit first run implemented, insert new user should be good, added framework for get all outfits.

--- DoneWithIt/app/utilities/requestData.js
+++ DoneWithIt/app/utilities/requestData.js
@@ -1,57 +1,137 @@
-import { sendEmail } from "./sendEmail"
 const KEY = 'nsGQLXniFr1RwE6idSX7fNOWIw5dZOWm3xV0TyyGTfbx5FOtQTbcyV8VDKyfYXsA';
-//
-export async function requestData(emailAddress) {
+
+/**
+ * 
+ * @param {*} emailAddress email address of registering user
+ * @returns false if user doesnt exist. User JSON object if user exists
+ */
+export async function userExists(emailAddress) {
+    //endpoint url
     const url = 'https://data.mongodb-api.com/app/data-ndazo/endpoint/data/v1/action/findOne';
 
-    const options = {
+    const opt = {
         method: 'POST',
         headers: {
             'content-type': 'application/json',
             'api-key': KEY,
         },
-        body: '{"dataSource": "DripCheckApp", "database": "test", "collection": "users", "filter": { "email": "' + emailAddress + '" }}'
+        body: {
+            "dataSource": "DripCheckApp",
+            "database": "test",
+            "collection": "users",
+            "filter" : {
+                "email": emailAddress
+            }
+        }
     };
+
+    //request database
+    var options = JSON.stringify(opt);
     let response = await fetch(url, options)
     let data = await response.json();
-    return data;
+    var user = JSON.parse(data);
+
+    //checks if JSON document is null, meaning user doesn't exist
+    if(user.document == null) {
+        return false;
+    } else {
+        //returns user document if true
+        return user;
+    }
 }
 
 export async function insertNewUser(email, displayName) {
-    var date = new Date();
-    var dd = String(date.getDate()).padStart(2, '0');
-    var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = date.getFullYear();
 
-    date = mm + '/' + dd + '/' + yyyy;
-     const url = 'https://data.mongodb-api.com/app/data-ndazo/endpoint/data/v1/action/insertOne';
+    //get current date
+    var date = getCurrentDate();
 
-     const options = {
-         method: 'POST',
-         headers: {
-             'content-type': 'application/json',
-             'api-key': 'nsGQLXniFr1RwE6idSX7fNOWIw5dZOWm3xV0TyyGTfbx5FOtQTbcyV8VDKyfYXsA',
-         },
-         body: '{"dataSource": "DripCheckApp", "database": "test", "collection": "users", "document": {"email": "' + email + '", "displayName": "' + displayName + '", "dateCreated": "' + date + '"}}'
+    //endpoint url
+    const url = 'https://data.mongodb-api.com/app/data-ndazo/endpoint/data/v1/action/insertOne';
+
+    const opt = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'api-key': 'nsGQLXniFr1RwE6idSX7fNOWIw5dZOWm3xV0TyyGTfbx5FOtQTbcyV8VDKyfYXsA',
+        },
+        body: {
+           "dataSource": "DripCheckApp",
+           "database": "test",
+           "collection": "users", 
+           "document": {
+                "email": email,
+                "displayName": displayName,
+                "dateCreated": date
+           }
+        }
      };
+     var options = JSON.stringify(opt);
      let response = await fetch(url, options)
      let data = await response.json();
      return data;
 }
 
-export async function getOutfits(emailAddress) {
+export async function addNewOutfit(email, displayName, description, imageString) {
+
+    var date = getCurrentDate();
+    const url = 'https://data.mongodb-api.com/app/data-ndazo/endpoint/data/v1/action/insertOne';
+
+    const opt = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'api-key': 'nsGQLXniFr1RwE6idSX7fNOWIw5dZOWm3xV0TyyGTfbx5FOtQTbcyV8VDKyfYXsA',
+        },
+        body: {
+            "dataSource": "DripCheckApp",
+            "database": "test",
+            "collection": "outfits", 
+            "document": {
+                "userEmail" : email,
+                "outfitName" : displayName,
+                "description" : description,
+                "dateCreated" : date,
+                "imageString" : imageString
+            }
+        }
+     };
+     var options = JSON.stringify(opt);
+     let response = await fetch(url, options)
+     let data = await response.json();
+     return data;
+}
+
+export async function getAllOutfits(emailAddress) {
+
     //TODO: Make db function to gather all outfits belonging to a specific user
     const url = 'https://data.mongodb-api.com/app/data-ndazo/endpoint/data/v1/action/getOutfits';
 
-    const options = {
+    const opt = {
         method: 'POST',
         headers: {
             'content-type': 'application/json',
             'api-key': KEY,
         },
-        body: '{"dataSource": "DripCheckApp", "database": "test", "collection": "users", "filter": { "email": "' + emailAddress + '" }}'
+        body: {
+            "dataSource": "DripCheckApp",
+            "database": "test",
+            "collection": "outfits",
+            "filter" : {
+                "email": emailAddress
+            }
+        }
     };
+    var options = JSON.stringify(opt);
     let response = await fetch(url, options)
     let data = await response.json();
     return data;
+}
+
+function getCurrentDate() {
+    var date = new Date();
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = date.getFullYear();
+    date = mm + '/' + dd + '/' + yyyy;
+    return date;
 }
