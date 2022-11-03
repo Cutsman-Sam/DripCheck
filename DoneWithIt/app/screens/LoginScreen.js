e54755d834f8c455134c20baa797b37bd1985d98
JoeySpencer26 <spenc173@purdue.edu>
2022-11-03 19:52:23 -0400

    Getting user data after following agreement

--- DoneWithIt/app/screens/LoginScreen.js
+++ DoneWithIt/app/screens/LoginScreen.js
@@ -42,20 +42,6 @@ function LoginScreen({navigation}) {
         let data = await response.json();
         global.userEmail = data.email;
         global.displayName = (String) (global.userEmail).substring(0, (String) (global.userEmail).indexOf("@"));
-
-        //console.log("LoginScreen: called userExists");
-        let previousData = await userExists(global.userEmail)
-        
-        //if user exists do nothing, else add them to database.
-        if(previousData == false) {
-          //console.log("LoginScreen: called insertNewUser");
-          global.accountDate = getCurrentDate();
-          insertNewUser(global.userEmail, global.displayName);
-        } else {
-          //Utilize previousData to load user's stuff
-          global.accountDate = JSON.parse(JSON.stringify(previousData)).document.dateCreated
-        }
-        //requestDatabase(2,data.email,data.email,"10-19-2020");
       }
       fetchData()
      
@@ -74,7 +60,7 @@ function LoginScreen({navigation}) {
               <Paragraph>Dripcheck collects certain data about the user, such as uploaded photos, email addresses, and liked posts. Do you consent to this collection of data?</Paragraph>
             </Dialog.Content>
             <Dialog.Actions>
-              <Button onPress={() => {setVisible(false); navigation.navigate("BottomTabs")}}>Accept</Button>
+              <Button onPress={() => {setVisible(false); navigation.navigate("BottomTabs"); handleLogin()}}>Accept</Button>
               <Button onPress={hideDialog}>Decline</Button>
             </Dialog.Actions>
           </Dialog>
@@ -84,6 +70,20 @@ function LoginScreen({navigation}) {
 }
 //<Button onPress={showDialog}>Show Dialog</Button>
 
+async function handleLogin(){
+    //console.log("LoginScreen: called userExists");
+    let previousData = await userExists(global.userEmail)
+        
+    //if user exists do nothing, else add them to database.
+    if(previousData == false) {
+      //console.log("LoginScreen: called insertNewUser");
+      global.accountDate = getCurrentDate();
+      insertNewUser(global.userEmail, global.displayName);
+    } else {
+      //Utilize previousData to load user's stuff
+      global.accountDate = JSON.parse(JSON.stringify(previousData)).document.dateCreated
+    }
+}
 const styles = StyleSheet.create({
     container: {
       flex: 1,
