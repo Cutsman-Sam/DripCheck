22b4fab4e17b1a29d3c4d9adf23c7c88dda5a827
Vish Lella <lella@purdue.edu>
2022-11-13 21:47:23 -0500

    inspirationTab supports scrolling

--- DoneWithIt/app/screens/InspirationScreen.js
+++ DoneWithIt/app/screens/InspirationScreen.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';  //status-bar replaced with 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Switch } from 'react-native-paper';
+
+const InspirationScreen = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+}
 
 function InspirationScreen(props) {
     return (
     <View style={styles.container}>
-      <Text>Inspiration Tab Body (replace)</Text>
+      <Switch
+        //trackColor={{ false: "#767577", true:}}
+      
+      />
+
+      <ScrollView style={styles.scrollView}>
+      <Text style={styles.text}> 
+        THIS IS A TEST FOR SCROLLING. 
+        Place the definitions in an include (.h) file, and use the same file in both client and
+        server software
+        – Instead of defining individual hex values for each possible request and response,
+        define a “response” bit and use it in the definition of message types 
+        Having a separate function for communication allows extra messages to be sent that are
+        not in the request queue (e.g., a control message to delete a disk)
+      </Text>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
     );
@@ -18,6 +39,13 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    scrollView: {
+      backgroundColor: 'orange',
+      marginHorizontal: 20,
+    },
+    text: {
+      fontSize: 42,
+    }
 });
 
 export default InspirationScreen;
