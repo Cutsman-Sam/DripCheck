8cd7d52db542f87fc7b1fd52d0cd1e25ae00881d
JoeySpencer26 <spenc173@purdue.edu>
2022-12-02 10:51:15 -0500

    Text updates to 'following' when pressing button

--- DoneWithIt/app/utilities/UploadPost.js
+++ DoneWithIt/app/utilities/UploadPost.js
@@ -17,69 +17,90 @@ import {
 } from "../styles/postStyle";
 
 import { Button, Dialog, Paragraph, Portal } from "react-native-paper";
-import {updateUser, getCurrentDate, addNewOutfit} from "../utilities/requestData";
+import {
+  updateUser,
+  getCurrentDate,
+  addNewOutfit,
+} from "../utilities/requestData";
 
 /**
  * function to add a new outfit to the closet
- * @param {} o_name 
- * @param {*} o_image 
- * @param {*} o_tag 
+ * @param {} o_name
+ * @param {*} o_image
+ * @param {*} o_tag
  */
- function saveOutfitToCloset(o_name, o_image, o_tag) {
-    for(let i = 0; i < global.outfitArray.length; i++) {
-      if(global.outfitArray[i] != null && global.outfitArray[i].image == o_image) {
-        console.error("Outfit already saved to your closet!");
-        return;
-      }
+function saveOutfitToCloset(o_name, o_image, o_tag) {
+  for (let i = 0; i < global.outfitArray.length; i++) {
+    if (
+      global.outfitArray[i] != null &&
+      global.outfitArray[i].image == o_image
+    ) {
+      console.error("Outfit already saved to your closet!");
+      return;
     }
-    
-    //outfit JSON object
-    let outfit = {
-      id: "",
-      description: "",
-      name: o_name,
-      date: getCurrentDate(),
-      image: o_image,
-      tags: o_tag,
-      lastWorn: "0000-00-00"
-    };
-    //add to global outfit array
-    global.outfitArray.push(outfit);
-  
-    //add to DB
-    global.outfitArray[global.outfitArray.length - 1].id = addNewOutfit(global.userEmail, o_name, "", o_image, o_tag);
-  
-    //make sure closet reloads
-    //global.closetLoaded = null;
   }
 
-function updateFollowers(userName){
-    if(global.followingUsernames == "") {
-        global.followingUsernames = " " + userName;
-    } else if (global.followingUsernames.includes(userName)) {
-        return;
-    } else {
-      global.followingUsernames = global.followingUsernames + ", " + userName;
-    }
-    updateUser(global.userEmail, global.displayName, global.accountDate, global.calendarStreak, 
-        "00-00-0000", global.oCount, global.pfp64, "undefined",global.followingUsernames);
+  //outfit JSON object
+  let outfit = {
+    id: "",
+    description: "",
+    name: o_name,
+    date: getCurrentDate(),
+    image: o_image,
+    tags: o_tag,
+    lastWorn: "0000-00-00",
+  };
+  //add to global outfit array
+  global.outfitArray.push(outfit);
+
+  //add to DB
+  global.outfitArray[global.outfitArray.length - 1].id = addNewOutfit(
+    global.userEmail,
+    o_name,
+    "",
+    o_image,
+    o_tag
+  );
+
+  //make sure closet reloads
+  //global.closetLoaded = null;
+}
+
+function updateFollowers(userName) {
+  if (global.followingUsernames == "") {
+    global.followingUsernames = " " + userName;
+  } else if (global.followingUsernames.includes(userName)) {
+    return;
+  } else {
+    global.followingUsernames = global.followingUsernames + ", " + userName;
+  }
+  updateUser(
+    global.userEmail,
+    global.displayName,
+    global.accountDate,
+    global.calendarStreak,
+    "00-00-0000",
+    global.oCount,
+    global.pfp64,
+    "undefined",
+    global.followingUsernames
+  );
 }
 
 const uploadPost = ({ item }) => {
-
-    const [isSaved, setIsSaved] = React.useState(false);
-
+  const [isSaved, setIsSaved] = React.useState(false);
+  const [followText, setFollowText] = React.useState("Follow");
   saveIcon = isSaved ? "heart" : "heart-outline";
   saveIconColor = isSaved ? "#2e64e5" : "#333";
 
   const [visible, setVisible] = React.useState(false);
   const hideDialog = () => setVisible(false);
 
-//  if(isSaved == true) {  //FIX: update saves globally
-//    item.saves = item.saves + 1;
-//     //global.postArray[id].saves = global.postArray[id].saves + 1;
-//     saveOutfitToCloset(item.userName + "'s Post", item.postImg, item.tags);        
-//   }
+  //  if(isSaved == true) {  //FIX: update saves globally
+  //    item.saves = item.saves + 1;
+  //     //global.postArray[id].saves = global.postArray[id].saves + 1;
+  //     saveOutfitToCloset(item.userName + "'s Post", item.postImg, item.tags);
+  //   }
 
   if (item.saves == 1) {
     saveText = "1 Save";
@@ -88,8 +109,13 @@ const uploadPost = ({ item }) => {
   } else {
     saveText = "Save";
   }
-
-
+  let userNames = global.followingUsernames.split(",");
+  for (let i = 0; i < userNames.length; i++) {
+    userNames[i] = userNames[i].trim();
+    if (userNames[i] == item.userName && followText == "Follow") {
+      setFollowText("Following");
+    }
+  }
 
   return (
     <Card>
@@ -99,32 +125,61 @@ const uploadPost = ({ item }) => {
           <UserName> {item.userName} </UserName>
           <PostTime> {item.postTime} </PostTime>
         </UserInfoText>
-        <Button onPress={() => {updateFollowers(item.userName)}} >Follow</Button>
+        <Button
+          onPress={() => {
+            if (followText !== "following") {
+              setFollowText("Following");
+            }
+            updateFollowers(item.userName);
+          }}
+        >
+          {followText}
+        </Button>
       </UserInfo>
       <PostText> {item.post} </PostText>
       <PostImg source={{ uri: "data:image/png;base64," + item.postImg }} />
 
       <InteractionWrapper>
         <Interaction active={item.saved}>
-        <Ionicons onPress={() => {saveOutfitToCloset(item.userName + "'s Post", item.postImg, item.tags); setIsSaved(true)}} name={saveIcon} size={25} color={saveIconColor}/>
+          <Ionicons
+            onPress={() => {
+              saveOutfitToCloset(
+                item.userName + "'s Post",
+                item.postImg,
+                item.tags
+              );
+              setIsSaved(true);
+            }}
+            name={saveIcon}
+            size={25}
+            color={saveIconColor}
+          />
           <InteractionText active={item.saved}>{saveText}</InteractionText>
         </Interaction>
         <Interaction>
-          <Ionicons name="pricetags" size={25} onPress={() => {setVisible(true)}}/>
+          <Ionicons
+            name="pricetags"
+            size={25}
+            onPress={() => {
+              setVisible(true);
+            }}
+          />
           <InteractionText>Tags</InteractionText>
           <Portal>
-          <Dialog visible={visible} onDismiss={hideDialog} dismissable={false}>
-          <Dialog.Title textAlign="center" >Tags</Dialog.Title>
-          <Dialog.Content>
-            <Paragraph>
-                {item.tags}
-            </Paragraph>
-          </Dialog.Content>
-          <Dialog.Actions>
-            <Button onPress={hideDialog}>Done</Button>
-          </Dialog.Actions>
-        </Dialog>
-        </Portal>
+            <Dialog
+              visible={visible}
+              onDismiss={hideDialog}
+              dismissable={false}
+            >
+              <Dialog.Title textAlign="center">Tags</Dialog.Title>
+              <Dialog.Content>
+                <Paragraph>{item.tags}</Paragraph>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button onPress={hideDialog}>Done</Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
         </Interaction>
       </InteractionWrapper>
     </Card>
